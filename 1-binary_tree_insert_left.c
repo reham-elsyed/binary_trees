7abf66274cd2aa3c3cfe38e855514ca6ca828f60
reham-elsyed <rehamshepl666@gmail.com>
2024-03-28 14:26:11 -0700

    binary

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/*
+ *binary_tree_insert_left - function to ind=sert node in tree
+ *@parent: pointer to the parent node
+ *@value: value of data to he new node
+ *Return: new-node on success null on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *nw;
+
+	if (parent == NULL)
+		return (NULL);
+
+	nw = binary_tree_node(parent, value);
+	if (nw == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		nw->left = parent->left;
+		parent->left->parent = nw;
+	}
+	parent->left = nw;
+	return (nw);
+}
