235a824cf00ca7106d6add8bf25b43828705d2c4
reham-elsyed <rehamshepl666@gmail.com>
2024-03-28 14:14:46 -0700

    binary

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_node - Create binary tree
  * @parent: Abointer to root node
- * @Value: the value in the node
+ * @value: the value in the node
  *
  * Return: null in error, pointer to new node on success
  */
@@ -11,13 +11,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if(new_node == NULL)
-		return(NULL);
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->right = NULL;
 	new_node->left = NULL;
 
-	return(new_node);
+	return (new_node);
 }
